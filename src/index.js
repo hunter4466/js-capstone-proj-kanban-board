efa9e1f2950dd893ff2a3563acdee32b4b79208c
carloso0114 <carlossospinag@gmail.com>
2021-08-03 14:37:05 -0500

    initial commit for layout

--- src/index.js
+++ src/index.js
@@ -2,33 +2,61 @@ import './Assets/Styles/_styles.scss';
 import './Assets/Styles/modules/_boxes.scss';
 import './Assets/Styles/modules/_buttons.scss';
 import './Assets/Styles/modules/_inputs.scss';
-import setScores from './Assets/Javascript/post';
-import getScores from './Assets/Javascript/get';
-import buildStructure from './Assets/Javascript/buildHTML';
+// import setScores from './Assets/Javascript/post';
+// import getScores from './Assets/Javascript/get';
+// import buildStructure from './Assets/Javascript/buildHTML';
 
-const submitScoreBtn = document.getElementById('submit_score_btn');
-const nameInput = document.getElementById('name_input');
-const scoreInput = document.getElementById('score_input');
-const refreshBtn = document.getElementById('refresh_btn');
-const messageTag = document.getElementById('messageTag');
+const api1 = 'https://api.thecatapi.com/v1/images/search?breed_ids=abys';
+const api2 = 'https://api.thecatapi.com/v1/images/search?breed_ids=aege';
+const api3 = 'https://api.thecatapi.com/v1/images/search?breed_ids=abob';
+// const api4 = 'https://api.thecatapi.com/v1/images/search?breed_ids=amau';
+// const api5 = 'https://api.thecatapi.com/v1/images/search?breed_ids=amis';
+// const api6 = 'https://api.thecatapi.com/v1/images/search?breed_ids=bamb';
+// const api7 = 'https://api.thecatapi.com/v1/images/search?breed_ids=bslo';
+// const api8 = 'https://api.thecatapi.com/v1/images/search?breed_ids=cspa';
+// const api9 = 'https://api.thecatapi.com/v1/images/search?breed_ids=beng';
+const fetchPics = () => {
+  const img = document.querySelector('.catsImg1');
+  fetch(api1)
+    .then((response) => response.json())
+    .then((json) => {
+      const pTag = document.createElement('p');
+      const cat1title = document.querySelector('#cat1title');
+      cat1title.innerText = json[0].breeds[0].name;
+      pTag.innerText = json[0].breeds[0].name;
+      const imgTag = document.createElement('img');
+      imgTag.setAttribute('src', `${json[0].url}`);
+      imgTag.setAttribute('width', '200');
+      imgTag.setAttribute('height', '200');
+      img.append(imgTag);
+      img.append(pTag);
+    });
+  const img2 = document.querySelector('.catsImg2');
+  fetch(api2)
+    .then((response) => response.json())
+    .then((json) => {
+      const pTag = document.createElement('p');
+      pTag.innerText = json[0].breeds[0].name;
+      const imgTag = document.createElement('img');
+      imgTag.setAttribute('src', `${json[0].url}`);
+      imgTag.setAttribute('width', '200');
+      imgTag.setAttribute('height', '200');
+      img2.append(imgTag);
+      img2.append(pTag);
+    });
+  const img3 = document.querySelector('.catsImg3');
+  fetch(api3)
+    .then((response) => response.json())
+    .then((json) => {
+      const pTag = document.createElement('p');
+      pTag.innerText = json[0].breeds[0].name;
+      const imgTag = document.createElement('img');
+      imgTag.setAttribute('src', `${json[0].url}`);
+      imgTag.setAttribute('width', '200');
+      imgTag.setAttribute('height', '200');
+      img3.append(imgTag);
+      img3.append(pTag);
+    });
+};
 
-function loadScores() {
-  const resultJSON = getScores();
-  resultJSON.then((data) => {
-    buildStructure(data.result);
-  });
-}
-
-submitScoreBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  setScores(nameInput.value, scoreInput.value);
-  messageTag.innerHTML = 'Score Added ✓';
-  setTimeout(() => { messageTag.innerHTML = ''; }, 1000);
-});
-
-refreshBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  loadScores();
-});
-
-loadScores();
+fetchPics();
